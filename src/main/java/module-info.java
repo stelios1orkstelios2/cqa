fc366ca1a489eddd077ef1f63c7d12c2ee72d341
Stelios Palatidis <stelios.palatidis@proton.me>
2026-06-03 00:08:56 +0300

    init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module gr.uom.cqa {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens gr.uom.cqa to javafx.fxml;
+    exports gr.uom.cqa;
+}
