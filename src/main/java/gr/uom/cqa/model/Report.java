389e0c17458f9cede58ce365b5724fd2f1f59f09
thgani <herculetheodoul@gmail.com>
2026-06-03 18:30:27 +0300

    score calculation changes

--- src/main/java/gr/uom/cqa/model/Report.java
+++ src/main/java/gr/uom/cqa/model/Report.java
@@ -36,8 +36,41 @@ public class Report {
     public int getCyclomaticComplexity() { return cyclomaticComplexity; }
 
     public void calculateScore(int totalLines) {
-        int penaltyPerIssue = 5;
-        int totalPenalty = issues.size() * penaltyPerIssue;
-        finalScore = Math.max(0, 100 - totalPenalty);
+        double totalPenalty = 0;
+
+        for (Issue issue : issues) {
+            switch (issue.getSeverity()) {
+                case CRITICAL: 
+                    totalPenalty += 10.0; 
+                    break;
+                case WARNING:  
+                    totalPenalty += 5.0; 
+                    break;
+                case INFO:     
+                    totalPenalty += 2.0; 
+                    break;
+            }
+        }
+
+        if (numberOfMethods > 0) {
+            double avgCc = (double) cyclomaticComplexity / numberOfMethods;
+            if (avgCc > 10.0) { 
+                // Το 10 θεωρείται το όριο του καλού κώδικα. Πάνω από αυτό αρχίζουν τα "σπαγγέτι".
+                totalPenalty += 15.0;
+            } else if (avgCc > 5.0) {
+                totalPenalty += 5.0;
+            }
+        }
+
+        // 3. Εξισορρόπηση με βάση το μέγεθος του αρχείου (LoC)
+        if (totalLines > 0) {
+            // Μειώνουμε λίγο την ποινή για τα πολύ μεγάλα αρχεία
+            double sizeFactor = Math.log10(totalLines); 
+            if (sizeFactor > 1) {
+                totalPenalty = totalPenalty / (sizeFactor * 0.8);
+            }
+        }
+
+        finalScore = (int) Math.max(0, 100 - Math.round(totalPenalty));
     }
 }
